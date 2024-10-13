7f6dac58816e9d793b6ec8c23c95bff742c6d837
Max Markov <legkiyprocces@mail.ru>
2024-10-13 01:12:22 -0700

    Доделано?

--- bin/kt2.dart
+++ bin/kt2.dart
@@ -1,4 +1,7 @@
+import 'provider/api_provider.dart';
 
-void main(List<String> arguments) {
-  print('Дарова!');
+void main(List<String> arguments) async {
+  final provider = ApiProvider();
+  final product = await provider.getProductList();
+  print(product);
 }
